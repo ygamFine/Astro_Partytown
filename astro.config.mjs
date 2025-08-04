959f1e42600d9d02ca033680e96026b164aac080
yun.zhang <ygamfine@126.com>
2025-08-04 18:17:57 +0800

    ISR Test v5.7

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel({
-    isr: {
-      expiration: 30,
-    },
+    isr: true
   }),
   
   server: {
